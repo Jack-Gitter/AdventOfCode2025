451cba06fd7a78c9988c1f1382aa983ffd48fd8c
Jack Gitter <jack.a.gitter@gmail.com>
2025-12-03 17:48:31 -0500

    ok

--- 2/part1.c
+++ 2/part1.c
@@ -1,10 +1,11 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 typedef struct range {
-  long start;
-  long end;
+  int start;
+  int end;
 } range_t;
 
 char* read_file(char file_name[]) {
@@ -37,7 +38,7 @@ char* read_file(char file_name[]) {
   return file_contents;
 }
 
-range_t* get_ranges(char file_contents[], size_t* ranges_len) {
+range_t* get_ranges(char file_contents[], int* ranges_len) {
 
   char* result = file_contents;
   char target = ',';
@@ -79,13 +80,20 @@ int main() {
 
   char* file_contents = read_file("input.txt");
   // these are all the ranges, now what do we do with them?
-  size_t ranges_len;
+  int ranges_len;
   range_t* ranges = get_ranges(file_contents, &ranges_len);
-  printf("ranges_len: %zu\n", ranges_len);
+  printf("ranges_len: %d\n", ranges_len);
 
-  for (size_t i = 0; i < ranges_len; i++) {
-    for (size_t j = ranges[i].start; j <= ranges[i].end; j++) {
-      printf("%zu\n", j);
+  for (int i = 0; i < ranges_len; i++) {
+    for (int j = ranges[i].start; j <= ranges[i].end; j++) {
+      int num_digits = floor(log10(abs(j))) + 1;
+      char* num_str = malloc(num_digits + 1);
+      snprintf(num_str, num_digits + 1, "%d", j);
+      printf("%s\n", num_str);
+      if ((num_digits % 2) == 0) {
+        printf("even len string found\n");
+      }
+      free(num_str);
     }
   }
 
