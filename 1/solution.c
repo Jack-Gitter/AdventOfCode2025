057a00a4f1e3763f92e6d1f112203cfba0922c19
Jack Gitter <jack.a.gitter@gmail.com>
2025-12-02 19:30:59 -0500

    ok

---
+++ 1/solution.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LINE_LENGTH 10
+int main() {
+  int val = 50;
+  int res = 0;
+
+  char line[MAX_LINE_LENGTH];
+  FILE* file = fopen("input.txt", "r");
+
+  if (file == NULL) {
+    perror("cannot open file");
+    exit(EXIT_FAILURE);
+  }
+
+  while (fgets(line, MAX_LINE_LENGTH, file) != NULL) {
+    char letter = line[0];
+
+    int num = atoi(line + 1);
+
+    if (letter == 'L') {
+      val = ((val - num) % 100 + 100) % 100;
+    } else {
+      val = ((val + num) % 100 + 100) % 100;
+    }
+
+    if (val == 0) {
+      res += 1;
+    }
+  }
+  printf("%d\n", res);
+}
---
+++ 1/solution2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LINE_LENGTH 10
+int main() {
+  int val = 50;
+  int res = 0;
+
+  char line[MAX_LINE_LENGTH];
+  FILE* file = fopen("input.txt", "r");
+
+  if (file == NULL) {
+    exit(EXIT_FAILURE);
+  }
+
+  while (fgets(line, MAX_LINE_LENGTH, file) != NULL) {
+    char letter = line[0];
+
+    int num = atoi(line + 1);
+
+    for (int i = 0; i < num; i++) {
+      if (letter == 'L') {
+        if (val != 0) {
+          val -= 1;
+          if (val == 0) {
+            res += 1;
+          }
+        } else {
+          val = 99;
+        }
+      } else {
+        if (val != 99) {
+          val += 1;
+        } else {
+          val = 0;
+          res += 1;
+        }
+      }
+    }
+  }
+
+  printf("%d\n", res);
+}
