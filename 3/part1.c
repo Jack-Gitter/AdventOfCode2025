b18a329ad73d754b1649a49c01714aad15c92b81
Jack Gitter <jack.a.gitter@gmail.com>
2025-12-04 13:56:30 -0500

    nice

--- 3/part1.c
+++ 3/part1.c
@@ -38,49 +38,52 @@ typedef struct joltage {
   int second_num_idx;
 } joltage_t;
 
+joltage_t get_line_joltage(char line[]) {
+  joltage_t line_joltage = {};
+  for (int i = 0; i < strlen(line) - 1; i++) {
+    if (i == 0) {
+      line_joltage.first_num = line[i];
+      line_joltage.first_num_idx = i;
+    } else {
+      int new_num = line[i] - '0';
+      int current_num = line_joltage.first_num - '0';
+      if (new_num > current_num) {
+        line_joltage.first_num = line[i];
+        line_joltage.first_num_idx = i;
+      }
+    }
+  }
+  for (int i = line_joltage.first_num_idx + 1; i < strlen(line); i++) {
+    if (i == line_joltage.first_num_idx + 1) {
+      line_joltage.second_num = line[i];
+      line_joltage.second_num_idx = i;
+    } else {
+      int new_num = line[i] - '0';
+      int current_num = line_joltage.second_num - '0';
+      if (new_num > current_num) {
+        line_joltage.second_num = line[i];
+        line_joltage.second_num_idx = i;
+      }
+    }
+  }
+  return line_joltage;
+}
 int main() {
   char* file_contents = read_file("input.txt");
 
   char* iterator = file_contents;
   char* line;
+  long total_joltage = 0;
 
-  long total = 0;
   while ((line = strsep(&iterator, "\n")) != NULL) {
-    joltage_t line_joltage = {};
-    for (int i = 0; i < strlen(line) - 1; i++) {
-      if (i == 0) {
-        line_joltage.first_num = line[i];
-        line_joltage.first_num_idx = i;
-      } else {
-        int new_num = line[i] - '0';
-        int current_num = line_joltage.first_num - '0';
-        if (new_num > current_num) {
-          line_joltage.first_num = line[i];
-          line_joltage.first_num_idx = i;
-        }
-      }
-    }
-    for (int i = line_joltage.first_num_idx + 1; i < strlen(line); i++) {
-      if (i == line_joltage.first_num_idx + 1) {
-        line_joltage.second_num = line[i];
-        line_joltage.second_num_idx = i;
-      } else {
-        int new_num = line[i] - '0';
-        int current_num = line_joltage.second_num - '0';
-        if (new_num > current_num) {
-          line_joltage.second_num = line[i];
-          line_joltage.second_num_idx = i;
-        }
-      }
-    }
+    joltage_t line_joltage = get_line_joltage(line);
     char joltage[3];
     joltage[0] = line_joltage.first_num;
     joltage[1] = line_joltage.second_num;
     joltage[2] = '\0';
-
-    int result = strtol(joltage, NULL, 10);
-    total += result;
+    int max_joltage = strtol(joltage, NULL, 10);
+    total_joltage += max_joltage;
   }
 
-  printf("total is: %zu\n", total);
+  printf("total is: %zu\n", total_joltage);
 }
