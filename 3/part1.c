fdf8103bc74500b47041e7e70d1d8ee753ef93ec
Jack Gitter <jack.a.gitter@gmail.com>
2025-12-04 13:51:34 -0500

    nice

--- 3/part1.c
+++ 3/part1.c
@@ -1,1 +1,86 @@
-int main() {}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* read_file(char file_name[]) {
+  FILE* file = fopen(file_name, "r");
+
+  fseek(file, 0, SEEK_END);
+  long file_size = ftell(file);
+  fseek(file, 0, SEEK_SET);
+
+  char* file_contents = malloc(sizeof(char) * (file_size));
+
+  if (file_contents == NULL) {
+    fclose(file);
+    return NULL;
+  }
+
+  int chunk_size = 1;
+  int read = fread(file_contents, chunk_size, file_size, file);
+
+  if (read != file_size) {
+    fclose(file);
+    return NULL;
+  }
+
+  file_contents[file_size - 1] = '\0';
+
+  fclose(file);
+
+  return file_contents;
+}
+
+typedef struct joltage {
+  char first_num;
+  int first_num_idx;
+  char second_num;
+  int second_num_idx;
+} joltage_t;
+
+int main() {
+  char* file_contents = read_file("input.txt");
+
+  char* iterator = file_contents;
+  char* line;
+
+  long total = 0;
+  while ((line = strsep(&iterator, "\n")) != NULL) {
+    joltage_t line_joltage = {};
+    for (int i = 0; i < strlen(line) - 1; i++) {
+      if (i == 0) {
+        line_joltage.first_num = line[i];
+        line_joltage.first_num_idx = i;
+      } else {
+        int new_num = line[i] - '0';
+        int current_num = line_joltage.first_num - '0';
+        if (new_num > current_num) {
+          line_joltage.first_num = line[i];
+          line_joltage.first_num_idx = i;
+        }
+      }
+    }
+    for (int i = line_joltage.first_num_idx + 1; i < strlen(line); i++) {
+      if (i == line_joltage.first_num_idx + 1) {
+        line_joltage.second_num = line[i];
+        line_joltage.second_num_idx = i;
+      } else {
+        int new_num = line[i] - '0';
+        int current_num = line_joltage.second_num - '0';
+        if (new_num > current_num) {
+          line_joltage.second_num = line[i];
+          line_joltage.second_num_idx = i;
+        }
+      }
+    }
+    char joltage[3];
+    joltage[0] = line_joltage.first_num;
+    joltage[1] = line_joltage.second_num;
+    joltage[2] = '\0';
+
+    int result = strtol(joltage, NULL, 10);
+    total += result;
+  }
+
+  printf("total is: %zu\n", total);
+}
