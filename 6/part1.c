35495f7dae961a4f6bcce59a4d0ea0791305a355
Jack Gitter <jack.a.gitter@gmail.com>
2025-12-06 12:33:39 -0500

    nice

---
+++ 6/part1.c
@@ -0,0 +1,75 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* read_file(char file_name[]) {
+  FILE* file = fopen(file_name, "r");
+
+  fseek(file, 0, SEEK_END);
+  long file_size = ftell(file);
+  fseek(file, 0, SEEK_SET);
+
+  char* file_contents = malloc(sizeof(char) * (file_size));
+
+  if (file_contents == NULL) {
+    fclose(file);
+    return NULL;
+  }
+
+  int chunk_size = 1;
+  int read = fread(file_contents, chunk_size, file_size, file);
+
+  if (read != file_size) {
+    fclose(file);
+    return NULL;
+  }
+
+  file_contents[file_size - 1] = '\0';
+
+  fclose(file);
+
+  return file_contents;
+}
+
+int get_row_count(char file_contents[]) {
+
+  int lines_before_operators = 0;
+  for (int i = 0; i < strlen(file_contents); i++) {
+    if (file_contents[i] == '\n') {
+      lines_before_operators++;
+    }
+  }
+  return lines_before_operators;
+}
+
+int get_col_count(char file_contents[]) {
+
+  int column_count = 0;
+  bool processing_number = false;
+  bool current_char_is_num = false;
+
+  for (int i = 0; i < strlen(file_contents) && file_contents[i] != '\n'; i++) {
+    current_char_is_num = isdigit(file_contents[i]);
+    if (current_char_is_num && !processing_number) {
+      column_count++;
+      processing_number = true;
+    }
+    if (!current_char_is_num) {
+      processing_number = false;
+    }
+  }
+  return column_count;
+}
+
+int main() {
+
+  char* file_contents = read_file("input.txt");
+  int row_count = get_row_count(file_contents);
+
+  int column_count = get_col_count(file_contents);
+
+  printf("col cound is %d\n", column_count);
+  printf("line count before operators is %d\n", row_count);
+  free(file_contents);
+}
