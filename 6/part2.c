b41ccd63e5eb709c8f75b873e0baa6c309a46c9f
Jack Gitter <jack.a.gitter@gmail.com>
2025-12-07 09:40:55 -0500

    close

--- 6/part2.c
+++ 6/part2.c
@@ -1,9 +1,14 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 char* read_file(char file_name[]) {
   FILE* file = fopen(file_name, "r");
+
+  if (file == NULL) {
+    return NULL;
+  }
 
   fseek(file, 0, SEEK_END);
   long file_size = ftell(file);
@@ -51,45 +56,98 @@ int get_col_count(char file_contents[]) {
   return i;
 }
 
+int get_equation_count(char file_contents[]) {
+
+  int counter = 0;
+  bool processing_num = false;
+  for (int i = 0; file_contents[i] != '\n'; i++) {
+    if (isspace(file_contents[i])) {
+      processing_num = false;
+    } else {
+      if (processing_num != true) {
+        counter++;
+      }
+      processing_num = true;
+    }
+  }
+  return counter;
+}
+
 int main() {
 
   char* file_contents = read_file("input.txt");
   int row_count = get_row_count(file_contents) + 1;
-  int column_count = get_col_count(file_contents);
-  printf("row count %d col count %d\n", row_count, column_count);
+  int col_count = get_col_count(file_contents);
 
-  char** input = malloc(sizeof(char*) * row_count);
-  long** equations = malloc(sizeof(long*) * (row_count - 1));
+  char** input = malloc(sizeof(char*) * row_count - 1);
+
+  for (int i = 0; i < row_count; i++) {
+    input[i] = malloc(sizeof(char) * col_count);
+  }
 
   int row = 0;
   int col = 0;
-  for (int i = 0; file_contents[i] != '\0'; i++) {
-    col++;
+  int i = 0;
+
+  while (file_contents[i] != '\0') {
     if (file_contents[i] == '\n') {
       col = 0;
       row++;
-      continue;
+    } else {
+      input[row][col++] = file_contents[i];
     }
-    if (input[row] == NULL) {
-      input[row] = malloc(sizeof(char) * column_count);
-    }
-    input[row][col] = file_contents[i];
+    i++;
   }
 
-  // parse the input by looping just the other way, go column
-  // by column to get the numbers
+  int equation_count = get_equation_count(file_contents);
 
-  for (int i = 0; i <= column_count; i++) {
-    char num[row_count];
+  long** equations = malloc(sizeof(long*) * equation_count);
+
+  for (int i = 0; i < equation_count; i++) {
+    equations[i] = malloc(sizeof(long) * row_count);
+  }
+
+  int equation_row = 0;
+  int equation_col = 0;
+  for (int i = 0; i < col_count; i++) {
+    int append_count = 0;
+    char* str = malloc(sizeof(char) * row_count);
     for (int j = 0; j < row_count - 1; j++) {
-      num[j] = input[j][i];
-      if (equations[i] == NULL) {
-        equations[i] = malloc(sizeof(long) * row_count);
+      if (!isspace(input[j][i])) {
+        str[append_count++] = input[j][i];
       }
-      long number = strtol(num, NULL, 10);
-      equations[i][j] = number;
-      // printf("%c", input[j][i]);
+    }
+    str[append_count] = '\0';
+    if (strlen(str) != 0) {
+      long num = strtol(str, NULL, 10);
+      if (equation_col == row_count - 1) {
+        equation_col = 0;
+        equation_row++;
+      }
+      equations[equation_row][equation_col++] = num;
+    }
+  }
+
+  for (int i = 0; i < equation_count; i++) {
+    for (int j = 0; j < row_count - 1; j++) {
+      printf("%lu ", equations[i][j]);
     }
     printf("\n");
+  }
+
+  char* operators = malloc(sizeof(char) * col_count);
+
+  int opp_idx = 0;
+  for (int i = 0; i < col_count; i++) {
+
+    char item = input[row_count - 1][i];
+    if (!isspace(item)) {
+      operators[opp_idx++] = item;
+    }
+  }
+
+  for (int i = 0; i < col_count; i++) {
+
+    printf("%c ", operators[i]);
   }
 }
